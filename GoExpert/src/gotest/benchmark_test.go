736a4e2d7a14c7e6439396128f8a1aa5d2891f3d
RainbowMango <qdurenhongcai@gmail.com>
2019-02-25 17:26:34 +0800

    Add test for SetBytes

--- GoExpert/src/gotest/benchmark_test.go
+++ GoExpert/src/gotest/benchmark_test.go
@@ -3,6 +3,7 @@ package gotest_test
 import (
     "testing"
     "gotest"
+    "time"
 )
 
 func BenchmarkMakeSliceWithoutAlloc(b *testing.B) {
@@ -14,5 +15,12 @@ func BenchmarkMakeSliceWithoutAlloc(b *testing.B) {
 func BenchmarkMakeSliceWithPreAlloc(b *testing.B) {
     for i := 0; i < b.N; i++ {
         gotest.MakeSliceWithPreAlloc()
+    }
+}
+
+func BenchmarkSetBytes(b *testing.B) {
+    b.SetBytes(1024 * 1024)
+    for i := 0; i < b.N; i++ {
+        time.Sleep(1 * time.Second) // 模拟待测函数
     }
 }
