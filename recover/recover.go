bbffacfe533d65aab7db3f30f7cdcc73bc26804d
RainbowMango <renhongcai@huawei.com>
2020-04-02 16:53:31 +0800

    Add example for recover

--- recover/recover.go
+++ recover/recover.go
@@ -1,6 +1,9 @@
 package recover
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 // 测试recover能否在具名函数中使用
 func ForRecover() {
@@ -13,4 +16,21 @@ func NamedRecover() {
 	defer ForRecover()
 
 	panic("this is a panic")
+}
+
+// 测试runtime.Goexit()会不会触发 defer
+func GoExit() {
+	defer func() {
+		fmt.Printf("runtime.Goexit will call defer like panic.\n")
+	}()
+	runtime.Goexit()
+}
+
+func GoExitRecover() {
+	defer func() {
+		if err := recover(); err == nil {
+			fmt.Printf("runtime.Goexit() can not be recovered.\n")
+		}
+	}()
+	runtime.Goexit()
 }
