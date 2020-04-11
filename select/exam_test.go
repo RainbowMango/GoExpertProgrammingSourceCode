dc99c45f466da1fe467629232b206789a4135df1
RainbowMango <renhongcai@huawei.com>
2020-04-11 09:23:08 +0800

    Add select exam

---
+++ select/exam_test.go
@@ -0,0 +1,28 @@
+package _select
+
+// 结果：输出随机
+func ExampleSelectExam1() {
+	SelectExam1()
+}
+
+// 结果：函数阻塞
+func ExampleSelectExam2() {
+	SelectExam2()
+}
+
+func ExampleSelectExam3() {
+	SelectExam3()
+	// Output:
+	// 1
+}
+
+func ExampleSelectExam4() {
+	SelectExam4()
+	// Output:
+	// 1
+}
+
+// 结果：函数阻塞
+func ExampleSelectExam5() {
+	SelectExam5()
+}
