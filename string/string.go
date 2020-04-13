5b98cce2b49a8839e33c76245f8bba70ab049b70
RainbowMango <renhongcai@huawei.com>
2020-04-13 15:02:44 +0800

    add string connect benchmark

--- string/string.go
+++ string/string.go
@@ -12,3 +12,16 @@ func StringBackslash() {
 this is "RainbowMango".`
 	fmt.Println(s)
 }
+
+func StringConnectSample1() string {
+	var s string
+	s = s + "a" + "b"
+	return s
+}
+
+func StringConnectSample2() string {
+	var s string
+	s = s + "a"
+	s = s + "b"
+	return s
+}
--- string/string_test.go
+++ string/string_test.go
@@ -1,5 +1,7 @@
 package _string
 
+import "testing"
+
 /*
 TODO: 分析测试失败原因
 	// Output:
@@ -18,4 +20,16 @@ TODO: 分析测试失败原因
 */
 func ExampleStringBackslash() {
 	StringBackslash()
+}
+
+func BenchmarkStringConnectSample1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		StringConnectSample1()
+	}
+}
+
+func BenchmarkStringConnectSample2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		StringConnectSample2()
+	}
 }
