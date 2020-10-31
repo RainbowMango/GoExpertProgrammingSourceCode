08e4c3cde57bfd5e0e2fb9b3fcaaf5f10412a9b9
RainbowMango <renhongcai@huawei.com>
2020-10-31 18:07:20 +0800

    finish demo

--- gomock/reporter/reporter_test.go
+++ gomock/reporter/reporter_test.go
@@ -13,8 +13,19 @@ func TestSuitTravel(t *testing.T) {
 
 	fakeReporter := mockreporter.NewMockWeatherReporter(ctl)
 
+	// 期望调用Report()，且参数为"beijing"时输出"SUNNY"
+	fakeReporter.EXPECT().Report("beijing").Return("SUNNY")
+
 	// 测试1：晴朗天气适合出行
-	SuitTravel("beijing", fakeReporter)
+	if SuitTravel("beijing", fakeReporter) != true {
+		t.Errorf("expected true")
+	}
+
+	// 期望调用Report()，且参数为"shanghai"时输出"TYPHOON"
+	fakeReporter.EXPECT().Report("shanghai").Return("TYPHOON")
 
 	// 测试2：台风天气不适合出行
+	if SuitTravel("shanghai", fakeReporter) != false {
+		t.Errorf("expected false")
+	}
 }
