019cb32e91a1a812e86f85e84e2cb02e6bfb20c3
RainbowMango <qdurenhongcai@gmail.com>
2022-07-30 19:27:22 +0800

    Update generic demos
    Signed-off-by: RainbowMango <qdurenhongcai@gmail.com>

--- generics/quickstart/main.go
+++ generics/quickstart/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 // SumInts adds together the values of m.
 func SumInts(m map[string]int64) int64 {
 	var s int64
@@ -46,4 +50,12 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v and %v\n",
 		SumInts(ints),
 		SumFloats(floats))
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
 }
