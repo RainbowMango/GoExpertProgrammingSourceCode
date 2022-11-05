696d05876f52d176a4c316886910cb8c8e8bdf70
RainbowMango <qdurenhongcai@gmail.com>
2022-11-05 15:04:31 +0800

    Add function generic description
    Signed-off-by: RainbowMango <qdurenhongcai@gmail.com>

---
+++ generics/overall/function.go
@@ -0,0 +1,11 @@
+package overall
+
+// SumIntsOrFloats sums the values of map m. It supports both int64 and float64
+// as types for map values.
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
